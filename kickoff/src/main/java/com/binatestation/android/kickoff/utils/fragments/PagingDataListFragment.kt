c049836087cb824f7941a304fc7f15679ecd10c6
Raghu Krishnan R <rkrsmail@gmail.com>
2020-09-30 21:46:36 +0530

    Load state adapter change done

--- kickoff/src/main/java/com/binatestation/android/kickoff/utils/fragments/PagingDataListFragment.kt
+++ kickoff/src/main/java/com/binatestation/android/kickoff/utils/fragments/PagingDataListFragment.kt
@@ -1,16 +1,5 @@
 /*
  * Copyright (c) 2020. Binate Station Private Limited. All rights reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Last Updated at 8/4/20 7:52 PM.
  */
 
 package com.binatestation.android.kickoff.utils.fragments
@@ -25,7 +14,6 @@ import com.binatestation.android.kickoff.repository.models.EmptyStateModel
 import com.binatestation.android.kickoff.repository.models.NetworkState
 import com.binatestation.android.kickoff.repository.models.enums.Status
 import com.binatestation.android.kickoff.utils.adapters.NetworkLoadStateAdapter
-import com.binatestation.android.kickoff.utils.adapters.PagedRecyclerViewAdapter
 import com.binatestation.android.kickoff.utils.adapters.PagingDataRecyclerViewAdapter
 import com.binatestation.android.kickoff.utils.adapters.holders.EmptyStateViewHolder
 import kotlinx.android.synthetic.main.fragment_list.*
@@ -56,14 +44,29 @@ open class PagingDataListFragment<DataModelType : Any>(private val comparator: D
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        getRecyclerView()?.adapter = adapter.withLoadStateHeaderAndFooter(
-            header = NetworkLoadStateAdapter(),
-            footer = NetworkLoadStateAdapter()
-        )
         val adapterEmptyStateBinding = DataBindingUtil.bind<AdapterEmptyStateBinding>(
             empty_state
         )
         adapterEmptyStateBinding?.let { emptyStateViewHolder = EmptyStateViewHolder(it) }
+    }
+
+    fun withLoadStateHeader() {
+        getRecyclerView()?.adapter = adapter.withLoadStateHeader(
+            header = NetworkLoadStateAdapter()
+        )
+    }
+
+    fun withLoadStateFooter() {
+        getRecyclerView()?.adapter = adapter.withLoadStateFooter(
+            footer = NetworkLoadStateAdapter()
+        )
+    }
+
+    fun withLoadStateHeaderAndFooter() {
+        getRecyclerView()?.adapter = adapter.withLoadStateHeaderAndFooter(
+            header = NetworkLoadStateAdapter(),
+            footer = NetworkLoadStateAdapter()
+        )
     }
 
     fun setNetworkState(newNetworkState: NetworkState?) {
