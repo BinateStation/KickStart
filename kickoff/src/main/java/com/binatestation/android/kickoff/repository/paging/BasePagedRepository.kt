694a307bb435520299139d7751b90bbecf15026d
Raghu Krishnan R <rkrsmail@gmail.com>
2020-10-01 17:58:19 +0530

    paging data total page bug fixes

--- kickoff/src/main/java/com/binatestation/android/kickoff/repository/paging/BasePagedRepository.kt
+++ kickoff/src/main/java/com/binatestation/android/kickoff/repository/paging/BasePagedRepository.kt
@@ -1,16 +1,5 @@
 /*
  * Copyright (c) 2020. Binate Station Private Limited. All rights reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Last Updated at 9/4/20 11:57 AM.
  */
 
 package com.binatestation.android.kickoff.repository.paging
@@ -18,14 +7,15 @@ package com.binatestation.android.kickoff.repository.paging
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
-import androidx.paging.toLiveData
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
 import com.binatestation.android.kickoff.repository.models.ApiResponse
 import com.binatestation.android.kickoff.repository.models.PagedResponseModel
 
 @Deprecated(
     message = "BasePagedRepository is deprecated and has been replaced by BasePagingRepository",
     replaceWith = ReplaceWith(
-        "BasePagedRepository<DataModelType>",
+        "BasePagingRepository<DataModelType>",
         "com.binatestation.android.kickoff.repository.paging.BasePagingRepository"
     )
 )
@@ -41,9 +31,15 @@ abstract class BasePagedRepository<DataModelType : Any> {
             BaseDataSourceFactory(pageIndex, pageSize, getAllCallBack)
 
         // We use toLiveData Kotlin extension function here, you could also use LivePagedListBuilder
-        val livePagedList = sourceFactory.toLiveData(
-            pageSize = pageSize
-        )
+        val livePagedList = Pager(
+            PagingConfig(
+                pageSize = pageSize
+            ),
+            null,
+            sourceFactory.asPagingSourceFactory(
+                ArchTaskExecutor.getIOThreadExecutor().asCoroutineDispatcher()
+            )
+        ).liveData
 
         val refreshState = Transformations.switchMap(sourceFactory.sourceLiveData) {
             it.initialLoad
