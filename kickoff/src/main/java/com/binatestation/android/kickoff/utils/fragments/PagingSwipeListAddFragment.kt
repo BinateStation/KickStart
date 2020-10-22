cb444229230f1e35fbd0ec4e8d42a4188d7850df
Raghu Krishnan R <rkrsmail@gmail.com>
2020-10-22 14:02:56 +0530

    setOnAddClickListener function name changed

--- kickoff/src/main/java/com/binatestation/android/kickoff/utils/fragments/PagingSwipeListAddFragment.kt
+++ kickoff/src/main/java/com/binatestation/android/kickoff/utils/fragments/PagingSwipeListAddFragment.kt
@@ -1,16 +1,5 @@
 /*
  * Copyright (c) 2020. Binate Station Private Limited. All rights reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Last Updated at 8/4/20 8:10 PM.
  */
 
 package com.binatestation.android.kickoff.utils.fragments
@@ -44,7 +33,21 @@ open class PagingSwipeListAddFragment<DataModelType : Any>(comparator: DiffUtil.
      * Method to get on click callback for [FloatingActionButton] add
      * @param onClick callback<view View?, Unit>
      */
+    @Deprecated(
+        message = "Changed to setOnAddClickListener", replaceWith = ReplaceWith(
+            expression = "setOnAddClickListener()"
+        )
+    )
     fun setOnClickListener(onClick: (view: View?) -> Unit) {
         action_add?.let { it.setOnClickListener { v -> onClick(v) } }
     }
+
+    /**
+     * Method to get on click callback for [FloatingActionButton] add
+     * @param onClickListener callback<view View?, Unit>
+     */
+    fun setOnAddClickListener(onClickListener: View.OnClickListener) =
+        action_add?.setOnClickListener(
+            onClickListener
+        )
 }
